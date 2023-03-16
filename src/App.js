0b92b60409d2dae96f06372e4986c8793e07759c
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2023-03-16 18:23:26 -0400

    jobs list fully functional

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +24,122 @@ function App() {
 }
 
 export default App;
+=======
+import { useEffect } from 'react';
+import { JobsList } from './components/JobsList/jobsList'
+import React, { useReducer, useCallback } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setJobs':
+      return {...state, jobs: action.payload };
+    case 'setPage':
+      return {...state, page: action.payload };
+    case 'setWhat':
+      return {...state, what: action.payload };
+    case 'setWhere':
+      return {...state, where: action.payload };
+    case 'setDistance':
+      return {...state, distance: action.payload };
+    case 'setLocation0':
+      return {...state, location0: action.payload };
+    case 'setLocation1':
+      return {...state, location1: action.payload };
+    case 'setLocation2':
+      return {...state, location2: action.payload };
+    case 'setLocation3':
+      return {...state, location3: action.payload };
+    case 'setLocation4':
+      return {...state, location4: action.payload };
+    case 'setLocation5':
+      return {...state, location5: action.payload };
+    case 'setLocation6':
+      return {...state, location6: action.payload };
+    case 'setLocation7':
+      return {...state, location7: action.payload };
+    case 'setCategory':
+      return {...state, category: action.payload };
+    case 'setSalaryMin':
+      return {...state, salaryMin: action.payload };
+    case 'setSalaryMax':
+      return {...state, salaryMax: action.payload };
+    case 'setFullTime':
+      return {...state, full_time: action.payload };
+    case 'setPartTime':
+      return {...state, part_time: action.payload };
+    case 'setCompany':
+      return {...state, company: action.payload };
+  default:
+    throw new Error();
+  }
+}
+
+const getUrl = (page, what, where, distance, location0, location1, location2, location3, location4, location5, location6, location7, category, salary_min, salary_max, full_time, part_time, company) => {
+  let searchUrl='https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=76dbecca&app_key=cdfe40cea1339c14198b7f6468e24d10';
+  let params = [page, what, where, distance, location0, location1, location2, location3, location4, location5, location6, location7, category, salary_min, salary_max, full_time, part_time, company];
+  let values = ["page", "what", "where", "distance", "location0", "location1", "location2", "location3", "location4", "location5", "location6", "location7", "category", "salary_min", "salary_max", "full_time", "part_time", "company"]
+  let count = -1
+
+  params.forEach(param => {
+    count++
+
+    if(count <= 0) {
+      searchUrl = `https://api.adzuna.com/v1/api/jobs/gb/search/${param}?app_id=76dbecca&app_key=cdfe40cea1339c14198b7f6468e24d10`;
+    };
+    if (count > 0) {
+      if (param !== -1 && param !== '') {
+        searchUrl += `&${values[count]}=${param}`
+      }
+    }
+  })
+
+  return searchUrl
+};
+
+export function App() {
+    const initialState = {
+      jobs: {},
+      page: 1,
+      what: '',
+      where: '',
+      distance: -1,
+      location0: '',
+      location1: '',
+      location2: '',
+      location3: '',
+      location4: '',
+      location5: '',
+      location6: '',
+      location7: '',
+      category: '',
+      salary_min: -1,
+      salary_max: -1,
+      full_time: -1,
+      part_time: -1,
+      company: -1,
+    };
+  
+    const [state, dispatch] = useReducer(reducer, initialState);
+  
+    const fetchFilteredJobs = useCallback(async () => {
+      const url = getUrl(state.page, state.what, state.where, state.distance, state.location0, state.location1, state.location2, state.location3, state.location4, state.location5, state.location6, state.location7, state.category, state.salary_min, state.salary_max, state.full_time, state.part_time, state.company)
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({type: 'setJobs', payload: data})
+    }, [state.page, state.what, state.where, state.distance, state.location0, state.location1, state.location2, state.location3, state.location4, state.location5, state.location6, state.location7, state.category, state.salary_min, state.salary_max, state.full_time, state.part_time, state.company])
+  
+    useEffect( () => {
+      fetchFilteredJobs();
+    }, [fetchFilteredJobs, state.page, state.what, state.where, state.distance, state.location0, state.location1, state.location2, state.location3, state.location4, state.location5, state.location6, state.location7, state.category, state.salary_min, state.salary_max, state.full_time, state.part_time, state.company]);
+  
+  return (
+    <div>
+      <JobsList 
+        jobs={state.jobs}
+        page={state.page}
+        dispatch={dispatch}
+      />
+    </div>
+  );
+};
+>>>>>>> Stashed changes
---
+++ src/components/JobsList/jobsList.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TablePagination, TableHead, TableRow, TableSortLabel } from '@mui/material';
+import {SectionTitle, TablePaper } from '../styles';
+
+export const JobsList = ({ jobs, page, dispatch }) => {
+
+    console.log(page);
+
+    const handleChangePage = (e, newPage) => {
+        dispatch({type: 'setPage', payload: page+1000});
+    }
+
+    //Headers for jobType,                     
+    const headers = [
+        { id: 'Job Title', minWidth: 80, align: 'center' },
+        { id: 'Company', minWidth: 80, align: 'center' },
+        { id: 'Job Location', minWidth: 80, align: 'center'},
+        { id: 'Category', minWidth: 80, align: 'center'},
+        { id: 'Part Time/Full Time', minWidth: 80, align: 'center' },
+        { id: 'Salary Range', minWidth: 80, align: 'center'},
+    ]
+
+    //CrashTable sorting state
+    const [currentColumn, setCurrentColumn] = useState('');
+    const [orderColumn, setOrderColumn] = useState('');
+    const [order, setOrder] = useState('desc');
+
+    /* Sets state for orderColumn & order */
+    const handleSort = (columnId, sortValue) => {
+        setCurrentColumn(columnId);
+        if (orderColumn === sortValue) {
+            setOrder(order === 'asc' ? 'desc' : 'asc');
+        } else {
+            setOrderColumn(sortValue);
+            setOrder('desc');
+        }
+    };
+
+    /* Orders the data in the columns according to the state value in orderColumn, either asc or desc depending on order */
+    const filteredJobs = jobs?.results?.sort((a, b) => {
+        if (!orderColumn) {
+            return 0;
+        } else {
+            const valueA = orderColumn.split('.').reduce((o, i) => o[i], a);
+            const valueB = orderColumn.split('.').reduce((o, i) => o[i], b);
+    
+            if (order === 'asc') {
+                return valueA > valueB ? 1 : -1;
+            } else {
+                return valueA < valueB ? 1 : -1;
+            }
+        }
+    });
+    
+
+    const colName = (property) => {
+        // eslint-disable-next-line default-case
+        switch (property){
+        case 'Job Title':
+            return 'title';
+        case 'Company':
+            return 'company.display_name';
+        case 'Job Location':
+            return 'location.display_name';
+        case 'Category':
+            return 'category.label';
+        case 'Part Time/Full Time':
+            return 'contract_time';
+        case 'Salary Range':
+            return 'salary_min';
+        }
+      };
+
+    return (
+        <div>
+            { jobs?.results?.length ? (
+                <TablePaper elevation={0} variant="outlined">
+                    <TableContainer sx={{ maxHeight: 1000 }}>
+                        <SectionTitle variant="h5">{'Search Results'}</SectionTitle>
+                        <Table stickyHeader aria-label="sticky table">
+                        <TableHead>
+                            <TableRow >
+                                {headers.map((column) => (
+                                    !!(jobs?.results?.length) &&
+                                    <TableCell key={column.id}>
+                                        <TableSortLabel active={currentColumn === column.id} direction={order} onClick={() => handleSort(column.id, colName(column.id))}>
+                                            {column.id}
+                                        </TableSortLabel>
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        { filteredJobs?.length ? (
+                            <TableBody>
+                                {filteredJobs
+                                    .map((row, index) => (
+                                    <TableRow
+                                        key={index}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0, verticalAlign: 'top' } }}
+                                    >
+                                        <TableCell scope="row" sx={{ verticalAlign: 'top' }}>{row.title}</TableCell>
+                                        <TableCell sx={{ verticalAlign: 'top' }}>{row.company.display_name}</TableCell>
+                                        <TableCell sx={{ verticalAlign: 'top' }}>{`${row.location.display_name}, ${row.location.area[0]}`}</TableCell>
+                                        <TableCell sx={{ verticalAlign: 'top' }}>{row.category.label}</TableCell>
+                                        <TableCell sx={{ verticalAlign: 'top' }}>{row.contract_time}</TableCell>
+                                        <TableCell sx={{ verticalAlign: 'top' }}>{`$${row.salary_min}-$${row.salary_max}`}</TableCell>
+                                    </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        ) : null }
+                        </Table>
+                    </TableContainer>
+                    
+                    {!!(jobs?.results?.length) &&
+                        <TablePagination
+                        component="div"
+                        count={jobs.count}
+                        page={page-1}
+                        rowsPerPage={10}
+                        onPageChange={handleChangePage}
+                        />
+                    }
+                </TablePaper>
+            ) : null }
+        </div>
+    );
+};
