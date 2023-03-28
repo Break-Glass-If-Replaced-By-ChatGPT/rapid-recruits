d02d2100a523f43f488f68a7a2a2c809e3f191c5
Damian Ulloa <damian.ulloa@clover.com>
2023-03-28 10:37:55 -0700

    Added base format to ResultsPage and ResumePage

--- src/Pages/Resultspage.js
+++ src/Pages/Resultspage.js
@@ -1,10 +1,19 @@
 import React from "react";
 import {Box, Typography} from '@mui/material';
+import { DetailedJobView } from "../components/DetailedJobView/detailedJobView";
+import { DetailedViewPage } from "./DetailedViewPage";
 
 export default function Resultspage(params) {
+
     return (
-        <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'column' }}>
-            <Typography variant="h1">Results</Typography>
+        <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'column',}}>
+            <Box sx={{ height:'10vh',display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'column', textAlign: 'Center'}}>
+                <Typography variant="h1">Header</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'Column'}} justifyContent='center' alignContent='middle'
+            alignItems='center'>
+                {state.selectedJob != [] ? <h1>Full Page search Results</h1> : <DetailedViewPage/>}
+            </Box>
         </Box>
     )
 };
--- src/Pages/Resumepage.js
+++ src/Pages/Resumepage.js
@@ -3,8 +3,14 @@ import {Box, Typography} from '@mui/material';
 
 export default function Resumepage(params) {
     return (
-        <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'column' }}>
-            <Typography variant="h1">Application</Typography>
+        <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'column',}}>
+            <Box sx={{ height:'10vh',display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'column', textAlign: 'Center'}}>
+                <Typography variant="h1">Header</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', flexDirection: 'Column'}} justifyContent='center' alignContent='middle'
+            alignItems='center'>
+                <ResumeForm/>
+            </Box>
         </Box>
     )
 };
