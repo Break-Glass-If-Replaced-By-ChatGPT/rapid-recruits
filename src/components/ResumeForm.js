5cc8398cd32924e8fdab7c7b60f4a931b2788936
miavh123 <116587742+miavh123@users.noreply.github.com>
2023-03-29 13:32:29 -0400

    added MUI to implement form (#18)

--- src/components/ResumeForm.js
+++ src/components/ResumeForm.js
@@ -1,61 +1,118 @@
-import React, {useState}  from 'react'
+import React, { useState } from 'react';
+import { TextField, Button, Typography } from '@material-ui/core';
 
-export function ResumeForm(){
-    const [name, setName] = useState('');
-    const [dateOfBirth,setDateOfBirth] = useState('');
-    const [email, setEmail] = useState('');
-    const [phoneNumber,setPhoneNumber] =useState('');
-    const [resume, setResume] = useState(null);
-    const [coverLetter, setCoverLetter] = useState(null);
+export function ResumeForm() {
+  const [name, setName] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [resume, setResume] = useState(null);
+  const [coverLetter, setCoverLetter] = useState(null);;
+  const [submitted, setSubmitted] = useState(false);
 
-    const handleSubmit = (event) =>{
-        event.preventDefault();
-    }; // submit data????
-   
-    const handleResumeChange = (event) => {
-        const selectedFile = event.target.files[0];
-        if (selectedFile) {
-          setResume(selectedFile);
-        }
-      };
+  const handleResumeChange = (event) => {
+    const file = event.target.files[0];
+    setResume(file);
+  };
 
-    const handleCoverLetterChange = (event) => {
-        const selectedFile = event.target.files[0];
-        if (selectedFile) {
-          setCoverLetter(selectedFile);
-        }
-      };
+  const handleCoverLetterChange = (event) => {
+    const file = event.target.files[0];
+    setCoverLetter(file);
+  };
 
-    return (
-      <>
-        <form onSubmit ={handleSubmit} className = 'form-css' >
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)}/>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Construct form data
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('dateOfBirth', dateOfBirth);
+    formData.append('email', email);
+    formData.append('phoneNumber', phoneNumber);
+    formData.append('resume', resume);
+    formData.append('coverLetter', coverLetter);
+    ;
 
-        <label htmlFor='dateOfBirth'>Date of Birth </label>
-        <input type="text" id="dateOfBirth" value={dateOfBirth} onChange={(event) => setDateOfBirth(event.target.value)}/>
+    // Submit form data
+    fetch('/submit-resume', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        // Handle response
+        console.log('Success:', response);
+        setSubmitted(true);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error('Error:', error);
+      });
+  };
 
-        <label htmlFor='email'>Email</label>
-        <input type="text" id="email" value={email} onChange={(event)=> setEmail(event.target.value)}/>
+  if (submitted) {
+    return <Typography variant="h6">Thank you for your submission!</Typography>;
+  }
 
-        <label htmlFor='phoneNumber'>PhonenNumber</label>
-        <input type="tel" id="phoneNumber" value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value)}/>
-        
-        <label htmlFor='resume'>Resume</label>
-        <input type="file" id="resume" accept=".pdf,.doc,.docx" onChange={handleResumeChange} value={resume}/>
+  return (
+    <div style={{ display: 'flex' }}>
+      <form onSubmit={handleSubmit}>
+        <Typography variant="h6">Submit Your Resume</Typography>
+        <TextField
+          label="Name"
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <TextField
+          label="Date of Birth"
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          value={dateOfBirth}
+          onChange={(event) => setDateOfBirth(event.target.value)}
+        />
+        <TextField
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <TextField
+          label="Phone Number"
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          value={phoneNumber}
+          onChange={(event) => setPhoneNumber(event.target.value)}
+        />
+        <TextField
+          type="file"
+          label="Resume"
+          margin="normal"
+          fullWidth
+          onChange={handleResumeChange}
+        />
+        <TextField
+          type="file"
+          label="Cover Letter"
+          margin="normal"
+          fullWidth
+          onChange={handleCoverLetterChange}
+        />
 
-        <label htmlFor='coverLetter'>Cover Letter</label>
-        <input type='file' id='coverLetter' accept='.pdf,.doc,.docx' onChange={handleCoverLetterChange} value={coverLetter} />
-
-        <label htmlFor='coverLetterContent'>Cover Letter Content</label>
-        <textarea id='coverLetterContent' rows= '10'></textarea>
-
-        <button type='submit'>Submit</button>
-       
-       </form>
-       </>
-
-    );
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default ResumeForm;
