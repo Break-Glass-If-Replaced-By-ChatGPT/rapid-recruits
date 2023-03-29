01d2e47a93e2be18b386bdd11c4c9b338219f9d8
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2023-03-29 12:58:04 -0400

    REF/small styling changes for filters menu, to allow for better spacing

--- src/Pages/Resultspage.js
+++ src/Pages/Resultspage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Typography} from '@mui/material';
+import { Box } from '@mui/material';
 import { SearchNav } from '../components/NavBars/SearchNav';
 import { JobsList } from '../components/JobsList/jobsList';
 
