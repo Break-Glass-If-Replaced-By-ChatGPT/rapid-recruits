12c53e2786ecc75fb8af74ee880c9139e466a798
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2023-03-30 11:12:30 -0400

    FEAT/added reset button to filters

--- src/components/FilterForm/FilterForm.js
+++ src/components/FilterForm/FilterForm.js
@@ -39,11 +39,15 @@ export function FilterForm(){
         dispatch({type: 'setSalaryMax', payload: updated.maxSalary});
         updated=resetValues;
     };
+
+    const handleReset = () => {
+        dispatch({ type: "setWhere", payload: resetValues.where });
+        dispatch({ type: "setDistance", payload: resetValues.distance });
+        dispatch({ type: "setCategory", payload: resetValues.category });
+        dispatch({ type: "setSalaryMin", payload: resetValues.minSalary });
+        dispatch({ type: "setSalaryMax", payload: resetValues.maxSalary });
+    };
  
-
-
-
-
    return (
        <div>
            <Button color="primary" disabled={false} onClick={()=>setIsDrawerOpen(true)} size="md" variant="soft"> Filters </Button>
@@ -51,13 +55,15 @@ export function FilterForm(){
                <form onSubmit={(event)=>handleSubmit(event)}>
                    <Box p={2} textAlign='Left' display='flex' flexDirection={'column'}>
                        <Typography variant='h6'>Filters</Typography>
-                       <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="Where_input" label="Where" variant="outlined" onChange={(e)=>handleChange('where', e)}/>
-                       <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="Distance_input" label="Distance" variant="outlined" onChange={(e)=>handleChange('distance', e)}/>
-                       <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="Category_input" label="Category" variant="outlined" onChange={(e)=>handleChange('category', e)}/>
+                       <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="Where_input" label="Location" variant="outlined" onChange={(e)=>handleChange('where', e)}/>
+                       <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="Distance_input" label="Distance (km)" variant="outlined" onChange={(e)=>handleChange('distance', e)}/>
+                       <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="Category_input" label="Job Category" variant="outlined" onChange={(e)=>handleChange('category', e)}/>
                        <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="SalaryMin_input" label="Min Salary" variant="outlined" onChange={(e)=>handleChange('minSalary', e)}/>
                        <TextField sx={{ marginTop: 1, marginBottom: 1 }} p={2} id="SalaryMax_input" label="Max Salary" variant="outlined" onChange={(e)=>handleChange('maxSalary', e)}/>
-                       <Button color="primary" disabled={false} type='Submit' size="md" variant="soft"> Apply </Button>
-                   </Box>
+                            <Button
+                                sx={{ marginTop: 1, marginBottom: 1 }} color="primary" disabled={false} type="submit" size="md" variant="soft">Apply Filters</Button>
+                            <Button color="secondary" disabled={false} onClick={handleReset} size="md" variant="soft">Reset Filters</Button>
+                    </Box>
                </form>
            </Drawer>
        </div>
